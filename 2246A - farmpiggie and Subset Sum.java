506ac9fc2b2781575fe8d7e36c9ac9e9da8e3493
Divya <divyatanwar681@gmail.com>
2026-08-18 21:40:42 +0530

    Add files via upload

---
+++ 2246A - farmpiggie and Subset Sum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            for (int i = 1; i <= n; i += 2) {
+                System.out.print((i + 1) + " " + i);
+                if (i + 2 <= n) {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
